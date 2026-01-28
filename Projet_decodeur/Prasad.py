5761213885ec40c2fa33694c7906dfa1f0b48784
Aze-PA <prasadsarallad@gmail.com>
2026-01-28 12:59:24 +0100

    Update Prasad.py

--- Projet_decodeur/Prasad.py
+++ Projet_decodeur/Prasad.py
@@ -1,33 +1,11 @@
 def bin_vers_dec(b):
     n = 0
     for i in range(len(b)):
-        n += b[i] * 2**(n - i - 1)
+        n += b[i] * 2**(len(b) - i - 1)
     return n
+
 
 def dec_vers_bin(n):
-    if n == 0 :
-        return [0]  
-    b = []
-    while n > 0:
-        bits.append(n % 2)
-        n = n // 2  
-    return b[::-1]
-
-def hex_vers_dec(hex):
-    hex = hex.upper()
-    n = 0
-    puissance = 0
-    for elm in range(hex):
-        if "0" <= elm <= "9":
-            chiffre = int(elm)
-        elif "A" <= elm <= "F":
-            chiffre = ord(elm) - ord("A") + 10
-        n += chiffre * (16 ** puissance)
-        puissance += 1
-    return n
-
-def hex_vers_bin(hex):
-    n = hex_vers_dec(hex) 
     if n == 0:
         return [0]
     b = []
@@ -36,20 +14,54 @@ def hex_vers_bin(hex):
         n = n // 2
     return b[::-1]
 
+
+def hex_vers_dec(h):
+    n = 0
+    puissance = 0
+    for i in range(len(h) - 1, -1, -1):
+        elm = h[i]
+
+        if '0' <= elm <= '9':
+            chiffre = ord(elm) - ord('0')
+        elif 'A' <= elm <= 'F':
+            chiffre = ord(elm) - ord('A') + 10
+        elif 'a' <= elm <= 'f':   
+            chiffre = ord(elm) - ord('a') + 10
+        else:
+            return None
+
+        n += chiffre * (16 ** puissance)
+        puissance += 1
+
+    return n
+
+
+def hex_vers_bin(h):
+    n = hex_vers_dec(h)
+    if n == 0:
+        return [0]
+    b = []
+    while n > 0:
+        b.append(n % 2)
+        n = n // 2
+    return b[::-1]
+
+
 def bin_vers_chaine(b):
     c = ""
     for elm in b:
         c += str(elm)
     return c
 
+
 def fractionnaire_bin_vers_dec(b):
     n = 0
     for i in range(len(b)):
-        n += bits[i] * 2**-(i + 1)
+        n += b[i] * 2**-(i + 1)
     return n
 
-def est_representable_bin_signe(n, b):
-    min_val = -2**(b-1)
-    max_val = 2**(b-1) - 1
-    return min_val <= n <= max_val
 
+def est_representable_bin_signe(n, b):
+    min_val = -2**(b - 1)
+    max_val = 2**(b - 1) - 1
+    return min_val <= n <= max_val
