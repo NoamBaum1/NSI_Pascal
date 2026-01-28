6dc473909dfc89b03fe15f11f1c064b278c965f9
Aze-PA <prasadsarallad@gmail.com>
2026-01-28 12:38:40 +0100

    Refactor representation functions and add signed check

--- Projet_decodeur/entiers_en_machine.py
+++ Projet_decodeur/entiers_en_machine.py
@@ -13,8 +13,15 @@ def est_representable_bin(nb, b):
     return 0 <= nb < 2**b
 
 #instruction 18
+def est_representable_bin_signe(n, b):
+    min_val = -2**(b-1)
+    max_val = 2**(b-1) - 1
+    return min_val <= n <= max_val
+
+#instruction 19
 def est_representable_comp2(nb, b):
     if b <= 0:
         return False
     return -2**(b-1) <= nb < 2**(b-1)
+
 
