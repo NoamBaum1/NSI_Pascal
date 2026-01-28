76197a370680e15a03a54f0da56fbb664540ad27
Aze-PA <prasadsarallad@gmail.com>
2026-01-28 12:37:05 +0100

    Update entiers_en_machine.py

--- Projet_decodeur/entiers_en_machine.py
+++ Projet_decodeur/entiers_en_machine.py
@@ -1,3 +1,11 @@
+#fonction nécéssaire pour éxécuter les programmes : 
+def lst_to_str(L):
+    ch = ""
+    for elm in L:
+        ch = ch + str(elm)
+    return ch
+    
+
 #instruction 17
 def est_representable_bin(nb, b):
     if b <= 0:
@@ -9,3 +17,4 @@ def est_representable_comp2(nb, b):
     if b <= 0:
         return False
     return -2**(b-1) <= nb < 2**(b-1)
+
